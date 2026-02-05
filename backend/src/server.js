2016d6fa84b51c6e5e8c9770d926388826897ad3
Nana Mohamadi <adilnana78@gmail.com>
2026-02-05 16:15:55 +0000

    auth implemented & inngest

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,21 @@
 import express from "express"
 import path from "path"
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import cors from "cors";
+
+import { functions, inngest } from "./config/inngest.js";
 
 import { ENV } from "./config/env.js"
 import { connectDB } from "./config/db.js";
+
 
 const app = express()
 
 // Active le middleware Clerk pour gérer automatiquement l’authentification des utilisateurs (sessions, tokens, sécurité) sur toutes les requêtes
 app.use(clerkMiddleware())
-
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: permet l'envoie des requetes avec les cookies
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 const __dirname = path.resolve();
