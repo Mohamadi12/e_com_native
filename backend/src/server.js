2a6a48ec5860f887ef99ff1cfea92d202cba3038
Nana Mohamadi <adilnana78@gmail.com>
2026-02-05 17:00:10 +0000

    Fixed inngest

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,32 @@
-import express from "express"
-import path from "path"
-import { clerkMiddleware } from '@clerk/express'
+import express from "express";
+import path from "path";
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import cors from "cors";
 
 import { functions, inngest } from "./config/inngest.js";
 
-import { ENV } from "./config/env.js"
+import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
-
-const app = express()
+const app = express();
 
 // Active le middleware Clerk pour gérer automatiquement l’authentification des utilisateurs (sessions, tokens, sécurité) sur toutes les requêtes
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: permet l'envoie des requetes avec les cookies
-app.use("/api/inngest", serve({ client: inngest, functions }));
-
+app.use(
+  "/api/inngest",
+  serve({ client: inngest, functions, signingKey: ENV.INNGEST_SIGNING_KEY }),
+);
 
 const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json())
+app.use(express.json());
 
-
-app.get("/api",(req,res) =>{
-    res.send("Hello World")
-})
+app.get("/api", (req, res) => {
+  res.send("Hello World");
+});
 
 // Mettre en production votre app
 if (ENV.NODE_ENV === "production") {
