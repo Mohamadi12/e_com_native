744699eb23067f85d65633c840a160dbf6d2083a
Nana Mohamadi <adilnana78@gmail.com>
2026-02-05 09:59:42 +0000

    deployment method

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,27 @@
 import express from "express"
-
+import path from "path"
+import { ENV } from "./config/env.js"
 
 const app = express()
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json())
 
-app.get("/",(req,res) =>{
+app.get("/api",(req,res) =>{
     res.send("Hello World")
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+// Mettre en production votre app
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port http://localhost:${ENV.PORT}`)
 })
