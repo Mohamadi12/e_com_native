145bf6b9344386ac1b7fdd0e196ec502f6150a34
Nana Mohamadi <adilnana78@gmail.com>
2026-02-05 15:26:02 +0000

    Fixed database

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,7 +5,7 @@ dotenv.config({ quiet: true });
 export const ENV = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT,
-  DATABASE_URL: process.env.DATABASE_URL,
+  MONGO_URI: process.env.MONGO_URI,
   CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
   CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
