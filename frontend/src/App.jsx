00e96e48b1b198f5af9f036077ca9e18f4257f85
Nana Mohamadi <adilnana78@gmail.com>
2026-02-06 15:27:50 +0000

    Signed & config dasiUy and tanstack query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,37 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import LoginPage from "./pages/LoginPage";
+import { useAuth } from "@clerk/clerk-react";
+import DashboardPage from "./pages/DashboardPage";
+import ProductsPage from "./pages/ProductsPage";
+import OrdersPage from "./pages/OrdersPage";
+import CustomersPage from "./pages/CustomersPage";
+import DashboardLayout from "./layouts/DashboardLayout";
+
+import PageLoader from "./components/PageLoader";
 
 const App = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) return <PageLoader />;
+
   return (
-    <div>
-      App
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      {/* Show the user button when the user is signed in */}
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <Routes>
+      <Route
+        path="/login"
+        element={isSignedIn ? <Navigate to={"/dashboard"} /> : <LoginPage />}
+      />
+
+      <Route
+        path="/"
+        element={isSignedIn ? <DashboardLayout /> : <Navigate to={"/login"} />}
+      >
+        <Route index element={<Navigate to={"dashboard"} />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="customers" element={<CustomersPage />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import { SignIn } from "@clerk/clerk-react"
+
+const LoginPage = () => {
+  return (
+    <div className="h-screen hero">
+      <SignIn />
+    </div>
+  )
+}
+
+export default LoginPage
